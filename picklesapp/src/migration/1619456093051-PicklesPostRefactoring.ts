a2b915de4c0d8b1150eb2e1b88835ba3a9ad8a0a
root <snaap4@gmail.com>
2021-04-27 00:58:00 +0800

    setup migration for students table

---
+++ picklesapp/src/migration/1619456093051-PicklesPostRefactoring.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class PicklesPostRefactoring1619456093051 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+	    await queryRunner.query(`ALTER TABLE student RENAME COLUMN dob TO birthday`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+	    await queryRunner.query(`ALTER TABLE student RENAME COLUMN birthday TO dob`); //reverts changes back to ori.
+    }
+
+}
